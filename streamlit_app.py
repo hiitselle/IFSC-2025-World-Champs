0389e2080b9ea95921bfbc95ceda6c643042e0ea
hiitselle <146594537+hiitselle@users.noreply.github.com>
2025-08-16 09:59:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ elif(genderSel=="Female Lead Semis"):
     df = load_data("https://docs.google.com/spreadsheets/d/12i_7HsoRs74S0FtzN04Uu1WZeToU6AoyOidHAL6WcGE/export?format=csv&gid=1802658245")
  
 elif(genderSel=="Male Lead Final"):
-    df = load_data("https://docs.google.com/spreadsheets/d/1RVgQboeDCi1X2zEQdCzWqe9HYQTJpj5EumjFXf4qjN0/edit?gid=1842627779#gid=1842627779")    
+    df = load_data("https://docs.google.com/spreadsheets/d/1RVgQboeDCi1X2zEQdCzWqe9HYQTJpj5EumjFXf4qjN0/export?format=csv&gid=1842627779")    
     
 else:
     df = load_data("https://docs.google.com/spreadsheets/d/12i_7HsoRs74S0FtzN04Uu1WZeToU6AoyOidHAL6WcGE/export?format=csv&gid=919701499")
